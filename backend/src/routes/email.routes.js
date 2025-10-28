7e098ae03489f1cd933028f3ba90701368919702
Hammad bin Iftikhar <ihammad317@gmail.com>
2025-10-29 01:12:20 +0500

    Remove stale lowercase backend folder (normalize folder structure)

--- backend/src/routes/email.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const emailController = require('../controllers/emailController');
-
-router.post('/send', emailController.sendEmail);
-router.get('/job/:id', emailController.getJob);
-router.get('/test', emailController.testEmail);
-
-module.exports = router;
